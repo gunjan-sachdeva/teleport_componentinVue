ed9e6616d8adde1f18bf7ebc1aa721bb0b188d72
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2023-07-20 13:14:13 +0530

    Teleport_Component

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="footer">
+
+    <h1>teleport Component</h1>
+    <p>Copyright @reserved All rights</p>
+
+    </div>
+</template>
+<script>
+export default {
+    name:"FooterComp"
+}
+</script>
+<style scoped>
+h1{
+    background-color: blueviolet;
+    color: blanchedalmond;
+}
+.footer
+{
+    color: rgb(170, 126, 48);
+    position: absolute;
+    bottom: 0%;
+    background-color: antiquewhite;
+    width: 90%;
+    left: 0%;
+    padding-left: 10%;
+}
+</style>
---
+++ src/components/HomeComp.vue
@@ -0,0 +1,19 @@
+<template>
+   <teleport to="#footer">
+    <FooterComp />
+   </teleport>
+</template>
+<script>
+import FooterComp from './FooterComp.vue'
+
+export default {
+    name:"HomeComp",
+    components:{
+        FooterComp
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
